77b2fc1493ea377a69baf6ca5fab70f3bb967c88
jojibab <jojibabu1043@gmail.com>
2023-03-22 13:25:49 +0530

    URL-pattern at Application-Level

--- DJango/DemoProject/DemoProject/settings.py
+++ DJango/DemoProject/DemoProject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DemoApp1',
+	'DemoApp2',
 ]
 
 MIDDLEWARE = [
--- DJango/DemoProject/DemoProject/urls.py
+++ DJango/DemoProject/DemoProject/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.conf.urls import url #old
+from django.urls import path	#new
+from django.conf.urls import include;
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path("DemoApp1/",include("DemoApp1.urls")),
+	path("DemoApp2/",include("DemoApp2.urls"))
 ]
