b5c7d3bbf94a11b81ddfddc4907531fa670ee4ae
jojibab <jojibabu1043@gmail.com>
2023-03-22 11:09:45 +0530

    added welcom

--- DJango/FirstProject/FirstApp/views.py
+++ DJango/FirstProject/FirstApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse;
 
 # Create your views here.
+def display(request): #view-function
+	ss = "<center><h2 style='color:Blue;'>Hello User, Welcome to Django First-Project First-App</h2><hr /></center>";
+	return HttpResponse(ss);
