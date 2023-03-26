ddaf0619f748d654f33aba3bae1192e1d2b6158a
jojibab <jojibabu1043@gmail.com>
2023-03-26 10:47:03 +0530

    added firstapp code

--- DJango/ThirdProject/ThirdProject/urls.py
+++ DJango/ThirdProject/ThirdProject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from FirstApp import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('wishes3/', views.wishes3),
 ]
