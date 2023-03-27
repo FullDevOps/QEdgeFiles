653551676e0ab3241e4c1e9829fc46530d813f54
jojibab <jojibabu1043@gmail.com>
2023-03-28 01:53:34 +0530

    added student info data with faker module

--- DJango/StudentDBProject/StudentDBApp/views.py
+++ DJango/StudentDBProject/StudentDBApp/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from StudentDBApp.models import Student
 
 #Create your views here.
-def studentview(request):
+def studentview(request):		#old
     studentlist = Student.objects.order_by('marks')
     dict1={'studentlist':studentlist}
     return render(request,'StudentDBApp/students.html',context=dict1);
 
+
+from StudentDBApp.models import Student2;
+def student_homepage(request):				#new
+    #students= Student2.objects.all()
+    #students=Student2.objects.filter(marks__lt=35)
+    #students=Student2.objects.filter(name__startswith='A')
+    #students=Student2.objects.all().order_by('marks')  #ASC
+    students=Student2.objects.all().order_by('-marks')   #DESC
+    return render(request, 'StudentDBApp/index.html', {'students':students})
--- DJango/StudentDBProject/StudentDBProject/urls.py
+++ DJango/StudentDBProject/StudentDBProject/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from StudentDBApp import views;
+from django.urls import re_path
+from StudentDBApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentview/', views.studentview),
+    path('studenthomepage/', views.student_homepage),
+
+    re_path('^.*$', views.student_homepage),
 ]
