0553288e235f64b214221cce57c6e5d6932db334
jojibab <jojibabu1043@gmail.com>
2023-03-27 17:20:03 +0530

    added employee data tables in html page with bd access data

--- DJango/DBModelProject/DBModelApp/views.py
+++ DJango/DBModelProject/DBModelApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from DBModelApp.models import Employee
 
-# Create your views here.
+#Create your views here.
+def empdata(request):
+    emplist=Employee.objects.all() # select * from emp table
+    dict1={'emplist':emplist}
+    return render(request, 'DBModelApp/emp.html', context=dict1);
