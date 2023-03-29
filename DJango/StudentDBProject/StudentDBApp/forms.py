52cfaca0ca4bfbb3d7df94911188e08ade7ba10d
jojibab <jojibabu1043@gmail.com>
2023-03-29 12:53:55 +0530

    added ModelFormProject

--- DJango/StudentDBProject/StudentDBApp/forms.py
+++ DJango/StudentDBProject/StudentDBApp/forms.py
@@ -75,30 +75,30 @@ class SignupForm(forms.Form):
 
 '''
 	def clean_name(self):
-		print('validating name-field...');
-		inputname = self.cleaned_data['name'];
+		print('validating name-field...')
+		inputname = self.cleaned_data['name']
 		if len(inputname) < 5:
-			raise forms.ValidationError('Min. no-of-chars in name-field should be 5..!!');
-		return inputname;
+			raise forms.ValidationError('Min. no-of-chars in name-field should be 5..!!')
+		return inputname
 
 	def clean_rollno(self):
-		inputrollno = self.cleaned_data['rollno'];
-		print('Validating rollno-field...');
+		inputrollno = self.cleaned_data['rollno']
+		print('Validating rollno-field...')
 		if inputrollno == 0:
-			raise forms.ValidationError('Roll-number field cannot be EMPTY or ZERO...');
-		return inputrollno;
+			raise forms.ValidationError('Roll-number field cannot be EMPTY or ZERO...')
+		return inputrollno
 
 	def clean_email(self):
-		inputemail = self.cleaned_data['email'];
-		print("Validating email-field...");
+		inputemail = self.cleaned_data['email']
+		print("Validating email-field...")
 		if len(inputemail) < 8:
-			raise forms.ValidationError('Email-field cannot be EMPTY or less than 3-chars...');
-		return inputemail;
+			raise forms.ValidationError('Email-field cannot be EMPTY or less than 3-chars...')
+		return inputemail
 
 	def clean_feedback(self):
 		inputfeedback = self.cleaned_data['feedback']
-		print("Validating feedback-field...");
+		print("Validating feedback-field...")
 		if len(inputfeedback) < 3:
-			raise forms.ValidationError('Feedback-field cannot be less than 3-chars...');
+			raise forms.ValidationError('Feedback-field cannot be less than 3-chars...')
 		return inputfeedback
 		'''
