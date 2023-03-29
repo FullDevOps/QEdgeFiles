f0d3ec4c7b20ec2fd4e29a57aaaeb0b82ba384cd
jojibab <jojibabu1043@gmail.com>
2023-03-29 14:36:16 +0530

    added movies list add movie and home page all details about movie

--- DJango/MovieProject/MovieProject/urls.py
+++ DJango/MovieProject/MovieProject/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+from MovieApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('addmovie/', views.add_movie_view),
+    path('listmovies/', views.list_movie_view),
+    re_path('^.*$', views.index_view),
 ]
