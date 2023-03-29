6bbccf028ffec9f421a169907f200002846bfa90
jojibab <jojibabu1043@gmail.com>
2023-03-29 14:41:32 +0530

    added date widge

--- DJango/MovieProject/MovieApp/forms.py
+++ DJango/MovieProject/MovieApp/forms.py
@@ -1,8 +1,23 @@
 from django import forms;
 from MovieApp.models import Movies
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class MoviesForm(forms.ModelForm):
     #no-separate fields are required(taken from model-Movies-class)
+    releasedate = forms.DateField(widget=DateInput())
+    moviename = forms.CharField()
+    actor = forms.CharField()
+    actress = forms.CharField()
+    rating = forms.FloatField()
+
     class Meta:
-        model=Movies
-        fields='__all__'
+        model = Movies
+        fields = ('releasedate', 'moviename', 'actor', 'actress', 'rating');
+
+    class Meta:
+        # model=Movies
+        # fields='__all__'
+        model = Movies
+        fields = ('releasedate', 'moviename', 'actor', 'actress', 'rating');
