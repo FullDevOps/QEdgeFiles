735db27ac5292c3408c23347ee267e164dd9c7d1
jojibab <jojibabu1043@gmail.com>
2023-03-29 13:36:25 +0530

    added students tables

--- DJango/ModelFormProject/ModelFormApp/admin.py
+++ DJango/ModelFormProject/ModelFormApp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from ModelFormApp.models import Student
 
-# Register your models here.
-admin.site.register(Student)
+#Register your models here.
+#admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['id','name','marks']
+
+admin.site.register(Student,StudentAdmin)
