0da3ad2a66a695162dec758a26c54527ea5e5f98
jojibab <jojibabu1043@gmail.com>
2023-03-30 12:17:38 +0530

    Session Management by using Session-API ##(PageCount-Application)

--- DJango/SessionProject2/MyApps1/views.py
+++ DJango/SessionProject2/MyApps1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# Session-Application-1
+def page_count_view(request):
+    count = request.session.get('count', 0)
+    newcount = count + 1
+    request.session['count'] = newcount
+    print(request.session.get_expiry_age())  # check in server-console
+    print(request.session.get_expiry_date())
+    return render(request, 'MyApps1/pagecount.html', {'count': newcount})
--- DJango/SessionProject2/SessionProject2/urls.py
+++ DJango/SessionProject2/SessionProject2/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyApps1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # session-application-1
+    path('pagecount/', views.page_count_view),
+
 ]
