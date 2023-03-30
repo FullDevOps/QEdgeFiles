78823b003b63c8fe76582777c34874f5f8d8eff1
jojibab <jojibabu1043@gmail.com>
2023-03-31 00:56:31 +0530

    Adding Extra Employee Information also to Company DetailView

--- DJango/CBVProject/MyApps1/admin.py
+++ DJango/CBVProject/MyApps1/admin.py
@@ -16,7 +16,16 @@ from MyApps1.models import Company
 
 # Register your models here.
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ['name', 'location', 'ceo'];
+    list_display = ['name', 'location', 'ceo']
 
 
 admin.site.register(Company, CompanyAdmin)
+
+from MyApps1.models import Employee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['eno', 'name', 'salary', 'company']
+
+
+admin.site.register(Employee, EmployeeAdmin)
