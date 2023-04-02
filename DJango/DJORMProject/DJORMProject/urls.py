3ce7084cd8ec2096061bb738e6aafc1c3cb63f01
jojibab <jojibabu1043@gmail.com>
2023-04-02 16:15:32 +0530

    added ORM Project with base.html page

--- DJango/DJORMProject/DJORMProject/urls.py
+++ DJango/DJORMProject/DJORMProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from MyApps1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display/', views.display_view),
 ]
--- DJango/DJORMProject/MyApps1/admin.py
+++ DJango/DJORMProject/MyApps1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from MyApps1.models import Employee
+
 
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['empno', 'ename', 'job', 'sal', 'address'];
+
+
+# admin.site.register(Employee,EmployeeAdmin);
+admin.site.register(Employee);
